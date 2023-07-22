e4d6b77322ba17129820b5c31ae5735ca09d0580
Om <omghate9@gmail.com>
2023-07-22 15:31:37 +0530

    SpW-Case2

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/usersResult", userResultRoutes);
 
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
--- server/models/userResult.js
+++ server/models/userResult.js
@@ -14,6 +14,6 @@ const userResultSchema = new mongoose.Schema({
     // Result: {type: String, required: true}
 
 });
-const userResult = mongoose.model("userResult", userResultSchema);
+const userResult = mongoose.model("userResult1", userResultSchema);
 
 module.exports = userResult;
